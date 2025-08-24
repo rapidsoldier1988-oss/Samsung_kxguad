f8814d0742530926b721899f1c1125fc9511e5bf
rapidsoldier1988-oss <rapidsoldier1988@gmail.com>
2025-08-24 11:37:39 +0530

    Add files via upload
    first commit

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting KXGuard API Server...
+echo.
+echo Make sure you have Node.js installed and dependencies are installed with: npm install
+echo.
+echo Starting server on http://localhost:3000
+echo Press Ctrl+C to stop the server
+echo.
+npm start
+pause
